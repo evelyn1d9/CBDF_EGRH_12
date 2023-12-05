a1e79af3a4fdcf99c5e7aad9e86e60b379bbdc8a
evelyn1d9 <evelyn_1d9@hotmail.com>
2023-12-05 14:04:35 -0600

    first commit

---
+++ src/app/calculadora/calculadora.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-calculadora',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './calculadora.component.html',
+  styleUrl: './calculadora.component.css'
+})
+export class CalculadoraComponent {
+resultado = 0;
+numero1 = 0;
+numero2 = 0;
+sumarNumeros(){
+this.resultado = this.numero1+this.numero2;
+}
+restarNumeros(){
+  this.resultado = this.numero1-this.numero2;
+}
+multiplicarNumeros(){
+  this.resultado = this.numero1*this.numero2;
+}
+dividirNumeros(){
+  if(this.numero2 ==0)
+  alert('No se puede realizar la division');
+else 
+this.resultado = this.numero1/this.numero2;
+}
+}
